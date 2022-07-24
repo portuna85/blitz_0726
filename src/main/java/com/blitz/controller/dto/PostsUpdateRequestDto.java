5f6c9cbb5674a447a0742133fffac7664152493c
portuna85 <portuna85@gmail.com>
2022-07-24 16:41:11 +0900

    Posts 업데이트

--- src/main/java/com/blitz/controller/dto/PostsUpdateRequestDto.java
+++ src/main/java/com/blitz/controller/dto/PostsUpdateRequestDto.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class PostsUpdateRequestDto {
+
     private String title;
     private String content;
 
--- src/main/java/com/blitz/service/PostsService.java
+++ src/main/java/com/blitz/service/PostsService.java
@@ -9,10 +9,8 @@ import com.blitz.repository.PostsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Service
@@ -30,9 +28,7 @@ public class PostsService {
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
-
         posts.update(requestDto.getTitle(), requestDto.getContent());
-
         return id;
     }
 
@@ -40,7 +36,6 @@ public class PostsService {
     public void delete (Long id) {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
-
         postsRepository.delete(posts);
     }
 
@@ -48,7 +43,6 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
-
         return new PostsResponseDto(entity);
     }
 
